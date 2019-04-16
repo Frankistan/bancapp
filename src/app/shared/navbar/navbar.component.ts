d1ac4ba3836c468775b18de24cf42342d954aa63
Frankistan <fffernandez84@gmail.com>
2019-04-16 12:45:29 +0200

    Leccion 108

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Subject } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styles: []
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styles: []
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+	destroy = new Subject<any>();
+	user: User;
 
-  constructor() { }
+	constructor(
+		private store: Store<AppState>,
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.store.select('auth')
+			.pipe(
+				filter(auth => auth.user != null),
+				takeUntil(this.destroy))
+			.subscribe(state => {
+				this.user = state.user;
+			});
+	}
 
+	ngOnDestroy(): void {
+		this.destroy.next();
+	}
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Subject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filter, takeUntil } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
 
 @Component({
 	selector: 'app-sidebar',
 	templateUrl: './sidebar.component.html',
 	styles: []
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+
+	destroy = new Subject<any>();
+	user: User;
 
 	constructor(
-		private auth: AuthService) {
-
-	}
+		private auth: AuthService,
+		private store: Store<AppState>,
+	) { }
 
 	ngOnInit() {
+		this.store.select('auth')
+			.pipe(
+				filter(auth => auth.user != null),
+				takeUntil(this.destroy))
+			.subscribe(state => {
+				this.user = state.user;
+			});
 	}
 
-	logout(){
+	logout() {
 		this.auth.logout();
+	}
+
+	ngOnDestroy(): void {
+		this.destroy.next();
 	}
 }
